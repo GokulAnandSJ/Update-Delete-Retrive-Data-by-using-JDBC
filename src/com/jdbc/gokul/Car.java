7d869f54ffa8c2f24fcc9b680d4b4f5c77eba7d3
GokulAnandSJ <gokulanand0110@gmail.com>
2023-12-26 09:46:57 +0530

    Updated File

--- src/com/jdbc/gokul/Car.java
+++ src/com/jdbc/gokul/Car.java
@@ -52,7 +52,7 @@ class Car {
 		 * no need to changes on my Java code I can change it on my properties file.
 		 * 
 		 */
-		String qure = ("select * from jdbc.car where CID= "+cid);  //QUERY to retrieve the record.
+		String qure = ("select * from jdbc.car where CID= "+cid);  //QUERY to retrieve the record. And this also JDBC Step 4 "Execute the SQL Statement".
  
 		try {
 			FileReader read = new FileReader("config/configCar.properties"); //To read the properties file.
@@ -63,43 +63,47 @@ class Car {
 			String driver = prop.getProperty("Driver");
 
 			Class.forName(driver);  //JDBC Step 1 process "load the class".
-			Connection con = DriverManager.getConnection(url,username,pwd);
-			Statement smt =con.createStatement();
-			ResultSet rs =smt.executeQuery(qure);
-			while(rs.next()) {
+			Connection con = DriverManager.getConnection(url,username,pwd); //JDBC Step 2 "Establish the connection"
+			Statement smt =con.createStatement();   //JDBC Step 3 "Create the Statement."
+			ResultSet rs =smt.executeQuery(qure);   //JDBC Step 5 "ResultSet".
+			while(rs.next()) {  // next() method is used to retrieve the record from one by one. 
 				int id = rs.getInt("CID");
 				String name = rs.getString("CNAME");
 				double cost = rs.getDouble("COST");
 				String var = rs.getString("VARIENT");
 
-				System.out.println("ID :"+id+" Name :"+name+" Cost :"+cost+" Varient :"+var);
+				System.out.println("ID :"+id+" Name :"+name+" Cost :"+cost+" Varient :"+var);//Print all the retrieve element.
 			}
-			rs.close();  smt.close();
+			rs.close();  smt.close();//This is IMPORTANT process to close all the open source because safety purpose.
 			con.close();
 		} 
-		catch (Exception e) {
+		catch (Exception e) {  //Catch the exception.
 			e.printStackTrace();
 		}
 	}
-	public static void update() {
+	public static void update() { //AGAIN the Same process to update the data.
 		Properties prop = new Properties();
-		String dynamicQuery = "insert into jdbc.Car values(?,?,?,?)";
-		Scanner s = new Scanner(System.in);
+		String dynamicQuery = "insert into jdbc.Car values(?,?,?,?)";   //hear ?-> is used to what the column name on our data base order
+		Scanner s = new Scanner(System.in);								//and this query is used to insert the value
 		System.out.println("Enter Update Process");
 		System.out.println("Enter Car ID ");
 		int cid = s.nextInt();
 		System.out.println("Enter Car Name ");
 		String cname = s.next();
-		cname=cname.toUpperCase();
+		cname=cname.toUpperCase();          //Convert the user Entered value to UpperCase to Store the Data.
 		System.out.println("Enter Car Cost ");
 		double cost = s.nextDouble();
 		System.out.println("Enter Car Varient likewise Disel or Petrol or Electric  ");
 		String var = s.next();
-		var=var.toUpperCase();
+		var=var.toUpperCase();				//Convert the user Entered value to UpperCase to Store the Data.
 
 		s.close();
 
 		try {
+			/**
+			 * Same JDBC process to to read and all JDBC process work. 
+			 * 
+			 */
 			FileReader read = new FileReader("config/configCar.properties");
 			prop.load(read);
 			String username = prop.getProperty("UserName");
@@ -111,17 +115,17 @@ class Car {
 			Connection con = DriverManager.getConnection(url,username,pwd);
 			PreparedStatement pstmt = con.prepareStatement(dynamicQuery);
 
-			pstmt.setDouble(3, cost);
-			pstmt.setInt(1, cid);
-			pstmt.setString (2, cname);
+			pstmt.setDouble(3, cost);  //This is the 3rd-->? on the Query.
+			pstmt.setInt(1, cid);	   //This is the 1st-->? on the Query.
+			pstmt.setString (2, cname);//This is the 2nd-->? on the Query.
 
-			pstmt.setString(4,var);
+			pstmt.setString(4,var);    //This is the 4th-->? on the Query.
 
-			int rs =pstmt.executeUpdate();
+			int rs =pstmt.executeUpdate();  //executeUpdate () method is used to execute the Query and update the value.
 			if(rs>0)
-				System.out.println("Data Update Sucessfully ....");
+				System.out.println("Data Update Sucessfully ....");  //Data can be updated means print this success message
 			else
-				System.err.println("Data Not Updated...");
+				System.err.println("Data Not Updated...");  //or else print this error message.
 			pstmt.close(); con.close(); 
 		} 
 		catch (Exception e) {
@@ -129,12 +133,15 @@ class Car {
 		}
 	}
 	public static void delete() {
+		/**
+		 * Same all the Process to delete the Query also.
+		 */
 		Properties prop = new Properties();
 
 		Scanner s = new Scanner(System.in);
 		System.out.println("Enter the CID for Delete");
 		int cid = s.nextInt();
-		String query = "delete from jdbc.Car where cid="+cid;
+		String query = "delete from jdbc.Car where cid="+cid;  //QUERY to delete the record from table.
 		s.close();
 		try {
 			FileReader read = new FileReader("config/configCar.properties");
@@ -147,11 +154,11 @@ class Car {
 			Class.forName(driver);
 			Connection con = DriverManager.getConnection(url,username,pwd);
 			Statement stmt = con.createStatement();
-			int rs = stmt.executeUpdate(query);
+			int rs = stmt.executeUpdate(query);   	//executeUpdate () method is used to execute the Query and update
 			if(rs>0)
-				System.out.println("Record delete sucess");
+				System.out.println("Record delete sucess"); //Data data deleted sucessfully print this message
 			else
-				System.err.println("Invalid Id Please Check the ID once Correctly...");
+				System.err.println("Invalid Id Please Check the ID once Correctly...");  //if not deleted means print this error message.
 			stmt.close(); con.close();
 
 		}
